bf73178780e2f9e8eb6842de70148ca36ba9575b
Etsub-tech <etsubamha4@gmail.com>
2026-07-11 23:47:44 +0300

    fixed the coloring issue

--- src/home.jsx
+++ src/home.jsx
@@ -260,7 +260,7 @@ function Home(){
 
                 <div className='cards-column'>
                 <div>
-                    <div className='card'>
+                <div className='card'>
                 <div className="icon-circle"><svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M9 2v3H7a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2V2z"/><line x1="9" y1="12" x2="15" y2="12"/><line x1="9" y1="16" x2="13" y2="16"/></svg></div>
                 <h2>Modern EMR</h2>
                 <h3>Electronic Medical Records designed for clinical <br/> efficiency and accuracy.</h3>
